73381f10646549ba9c12ddfaf0d9b24af1005c56
Arsh150701 <akshatkakul@gmail.com>
2020-08-23 14:04:15 +0530

    Joy interface in progress

---
+++ app/src/main/java/com/sdp/remotehealthcareapp/JoyInterface.java
@@ -0,0 +1,14 @@
+package com.sdp.remotehealthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoyInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joy_interface);
+    }
+}
