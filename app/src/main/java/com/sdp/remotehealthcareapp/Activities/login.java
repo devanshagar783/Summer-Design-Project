27b9c51c8ce169fccdba4ca3434e4f519229eab2
Devansh <201851038@iiitvadodara.ac.in>
2020-10-13 01:42:12 +0530

    Added authentication in email sign up

---
+++ app/src/main/java/com/sdp/remotehealthcareapp/Activities/login.java
@@ -0,0 +1,54 @@
+package com.sdp.remotehealthcareapp.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.sdp.remotehealthcareapp.R;
+
+public class login extends AppCompatActivity {
+    private EditText email;
+    private EditText pw;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText)findViewById(R.id.emailid);
+        pw = (EditText)findViewById(R.id.pwid);
+        login = (Button)findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), pw.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    if(FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    }
+                                    else
+                                        Toast.makeText(getApplicationContext(), "Please verify email", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+    }
+}
