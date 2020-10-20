39589ff7b1ebef2de109bd51d737594a8e0d5ee9
Devansh <201851038@iiitvadodara.ac.in>
2020-10-20 20:11:37 +0530

    Fixed HealthFiles.java

--- app/src/main/java/com/sdp/remotehealthcareapp/Fragments/HealthFiles.java
+++ app/src/main/java/com/sdp/remotehealthcareapp/Fragments/HealthFiles.java
@@ -1,6 +1,7 @@
 package com.sdp.remotehealthcareapp.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,25 +17,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.sdp.remotehealthcareapp.Activities.AttachmentReportActivity;
 import com.sdp.remotehealthcareapp.R;
 
 import java.util.Objects;
 
 public class HealthFiles extends Fragment {
-    View V;
 
+    View V;
+    TextView userName;
     Button prescriptions;
     Button medical;
     Button at_repo;
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,14 +42,13 @@ public class HealthFiles extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         V=inflater.inflate(R.layout.fragment_healthfiles, container, false);
+        V = inflater.inflate(R.layout.fragment_healthfiles, container, false);
         init();
         button_listerns();
         return V;
     }
 
     private void button_listerns() {
-
         prescriptions.setOnClickListener(v -> {
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             ft.replace(R.id.fragment, new Prescriptions());
@@ -68,44 +63,20 @@ public class HealthFiles extends Fragment {
             ft.addToBackStack("Medical");
             ft.commit();
         });
-        at_repo.setOnClickListener(v -> {
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.replace(R.id.fragment, new At_repo());
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            ft.addToBackStack("Attachments");
-            ft.commit();
+        at_repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AttachmentReportActivity.class);
+                startActivity(intent);
+            }
         });
     }
 
-    private void init()
-    {
-
-        prescriptions= V.findViewById(R.id.button_prescriptions);
-        medical= V.findViewById(R.id.button_medical);
-        at_repo= V.findViewById(R.id.button_at_repo);
-        getName();
-
-
-    }
-    private void getName(){
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user !=null) {
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            TextView username = V.findViewById(R.id.text_user);
-            db.collection("users")
-                    .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if (task.isSuccessful()) {
-                                DocumentSnapshot document = task.getResult();
-                                String name= (document.getString("Name"));
-                                username.setText(name);
-
-                            }
-                        }
-                    });
-        }
+    private void init() {
+        userName = V.findViewById(R.id.text_user);
+        userName.setText(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getDisplayName());
+        prescriptions = V.findViewById(R.id.button_prescriptions);
+        medical = V.findViewById(R.id.button_medical);
+        at_repo = V.findViewById(R.id.button_at_repo);
     }
 }
